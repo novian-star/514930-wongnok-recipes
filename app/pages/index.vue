e29b71863c60976bc490572c5f2cb8fb0186df03
humanityisyour <humanityisyour@gmail.com>
2025-05-20 09:31:13 +0000

    ✨ feat(recipe): improve recipe fetching UI and endpoints

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,70 @@
+<script setup lang="ts">
+// Search
+const search = ref<string>('');
+const searching = ref<boolean>(false);
+const searchTimeout = ref<NodeJS.Timeout | null>(null);
+
+watch(search, () => {
+  if (searchTimeout.value) {
+    clearTimeout(searchTimeout.value);
+  }
+
+  searching.value = true;
+  searchTimeout.value = setTimeout(() => {
+    refresh();
+    searching.value = false;
+    page.value = 1;
+  }, 500);
+});
+
+// Pagination
+const page = ref<number>(1);
+const limit = ref<number>(10);
+const offset = computed(() => (page.value - 1) * limit.value);
+
+watch(page, () => refresh);
+
+// Data
+const { data, refresh } = await useAsyncData(async () => {
+  const result = await $fetch('/api/recipes', {
+    query: {
+      search: search.value,
+      limit: limit.value,
+      offset: offset.value,
+    },
+  });
+
+  return { recipes: result.data, count: result.meta.count };
+});
+
+const recipes = computed(() => data.value?.recipes);
+const count = computed(() => data.value?.count);
+</script>
+
 <template>
-  <div>Hello World!</div>
+  <div class="max-w-5xl p-4 mx-auto space-y-4">
+    <header>
+      <h1 class="text-2xl font-semibold">Recipes</h1>
+    </header>
+    <div class="space-y-4">
+      <!-- Search -->
+      <div>
+        <UInput v-model="search" type="text" :loading="searching" icon="lucide:search" />
+      </div>
+      <!-- Recipe list -->
+      <div>
+        <template v-for="recipe in recipes" :key="recipe.id">
+          <UCard>{{ recipe }}</UCard>
+        </template>
+      </div>
+      <!-- Pagination -->
+      <div v-if="(count || 0) > limit">
+        <UPagination
+          v-model:page="page"
+          :items-per-page="limit"
+          :total="count"
+        />
+      </div>
+    </div>
+  </div>
 </template>
--- server/api/recipes/index.get.ts
+++ server/api/recipes/index.get.ts
@@ -1,13 +1,44 @@
 import { prisma } from '~~/server/libs/prisma';
 
-export default defineEventHandler(async (_event) => {
-  return await prisma.recipe.findMany({
-    include: {
-      ratings: {
-        select: {
-          value: true,
+export default defineEventHandler(async (event) => {
+  const { search, limit, offset } = getQuery(event);
+
+  const [recipes, count] = await prisma.$transaction([
+    prisma.recipe.findMany({
+      where: {
+        OR: search ? [
+          { title: { contains: String(search) } },
+          { description: { contains: String(search) } },
+          { ingredients: { contains: String(search) } },
+          { instructions: { contains: String(search) } },
+        ] : undefined,
+      },
+      include: {
+        ratings: {
+          select: {
+            value: true,
+          },
         },
       },
+      take: limit ? parseInt(String(limit)) || 10 : 10,
+      skip: offset ? parseInt(String(offset)) || 0 : 0,
+    }),
+    prisma.recipe.count({
+      where: {
+        OR: search ? [
+          { title: { contains: String(search) } },
+          { description: { contains: String(search) } },
+          { ingredients: { contains: String(search) } },
+          { instructions: { contains: String(search) } },
+        ] : undefined,
+      },
+    }),
+  ]);
+
+  return {
+    data: recipes,
+    meta: {
+      count,
     },
-  });
+  };
 });
